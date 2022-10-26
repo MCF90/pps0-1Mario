66e014df21977044735541fd67323316bb816da3
Cesar vazquez <cvazquezm09@informatica.iesvalledeljerteplasencia.es>
2022-10-26 19:49:02 +0200

    Ultima modificacion

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 </head>
 <body>
 Hola mundo Mario. Prueba para push.
+<img border='0' src='//lh5.ggpht.com/_qgZA1ny_dAs/TM2UGNH7ORI/AAAAAAAAEuc/yRvKD_gAIg8/spiders.gif' style='position:fixed; top:0; left:0;'/>
 <p>
   Crea un enlace a
   <a href="cesar.html">la página de Cesar</a>.
